ac1cd99aff14880391f11c0fa2578b3d7d8687d5
zhr85210078 <zhr85210078@163.com>
2017-09-01 17:28:41 +0800

    ydtnodejs

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(favicon(config.server.faviconUrl));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(config.server.cookieSecret));
-//app.use(log.morgan);
-//app.use(log.log4js);
-app.use("/src",express.static(config.server.staticUrl));
+app.use(log.morgan);
+app.use(log.log4js);
+app.use('/src', express.static(config.server.staticUrl));
 
 app.use('/', routes);
 
