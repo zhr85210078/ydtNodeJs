fc49e805e683f4a55eba1849423119db88797080
zhr85210078 <zhr85210078@163.com>
2017-09-01 17:28:00 +0800

    ydtnodejs

--- app.js
+++ app.js
@@ -23,12 +23,11 @@ require('./lib/config/tmplConfig').tmplConfig(app, config.server.tmplUrl);//nunj
 app.use(favicon(config.server.faviconUrl));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser(config.vProfile.cookieSecret));
+app.use(cookieParser(config.server.cookieSecret));
 //app.use(log.morgan);
 //app.use(log.log4js);
 app.use("/src",express.static(config.server.staticUrl));
 
-app.all('*', routes.requireAuthentication);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
       listenPort: 4000,                                   
       tmplUrl: path.join(__dirname, 'src/views'),      
       staticUrl: path.join(__dirname, 'src'),
-      faviconUrl: path.join(__dirname, 'src/img', 'logo.ico')                         
+      faviconUrl: path.join(__dirname, 'src/img', 'logo.ico'),
+      cookieSecret:"Secret"                         
     }
   };
